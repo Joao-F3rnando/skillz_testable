40848e849eef63dabe1db7429845341ddd05a149
Joao-F3rnando <106031518+Joao-F3rnando@users.noreply.github.com>
2023-09-19 19:25:50 -0300

    fixing email and feedback inputs

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -6,25 +6,28 @@ export const Feedback = ({ url }) => {
     const [email, setEmail] = useState('')
 
     const handleSubmitFeedback = async () => {
-        const sendFeedback = {
-            email,
-            feedback
+        if(email != '' && feedback != '')
+        {
+            const sendFeedback = {
+                email,
+                feedback
+            }
+    
+            await fetch(`${url}/update`, {
+                method: 'POST',
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(sendFeedback)
+            })
         }
-
-        await fetch(`${url}/update`, {
-            method: 'POST',
-            headers: {
-                "content-type": "application/json"
-            },
-            body: JSON.stringify(sendFeedback)
-        })
     }
 
     return (
         <div className='feedback'>
             <div className='inputs'>
-                <label>Email: <input className='email' required type="email" value={email} onChange={(e) => setEmail(e.target.value)} /></label>
-                <label>Feedback: <input className='comment' required type="text" value={feedback} onChange={(e) => setFeedback(e.target.value)} /></label>
+                <label>Email: <input className='email' type="email" value={email} onChange={(e) => setEmail(e.target.value)} /></label>
+                <label>Feedback: <input className='comment' type="text" value={feedback} onChange={(e) => setFeedback(e.target.value)} /></label>
             </div>
             <button onClick={() => handleSubmitFeedback()}>Enviar avaliação</button>
         </div>
